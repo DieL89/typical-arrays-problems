ead7090d1fe427dd5bfb6a0798f9e6f1f2a8cbe9
DieL89 <dzmitry273@gmail.com>
2021-03-08 14:48:01 +0300

    feat: add func min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length ? Math.min(...array) : 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length ? Math.max(...array) : 0);
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length) {
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+  }
+
   return 0;
 }
